19586bb9f126d9908da1d812d0da949f3a5b1c1a
dib-a <96333029+dib-a@users.noreply.github.com>
2022-06-02 22:07:41 +0200

    Update install-teamviewer.sh

--- install-teamviewer.sh
+++ install-teamviewer.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-cd ~
 git clone https://aur.archlinux.org/teamviewer.git
 cd teamviewer
 sudo pacman -Sy qt5-webkit
@@ -8,3 +7,4 @@ makepkg -si
 sudo teamviewer --daemon enable
 systemctl enable teamviewerd
 systemctl start teamviewerd
+sudo rm -r teamviewer
